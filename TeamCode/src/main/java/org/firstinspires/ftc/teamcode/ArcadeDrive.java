1728803ab8e20e66a6ea7713d301ca4016f00542
Jeff Fansler <JeffF@FanzooTechnology.com>
2018-01-29 18:34:00 -0500

    added arcade drive one stick

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArcadeDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArcadeDrive.java
@@ -7,17 +7,22 @@ import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Gyroscope;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
 
 @TeleOp
-public class MyFIRSTJavaOpMode extends LinearOpMode {
+public class ArcadeDrive extends LinearOpMode {
     private DcMotor right;
     private DcMotor left;
+    private float leftPower;
+    private float rightPower;
+    private float xValue;
+    private float yValue;
 
     @Override
     public void runOpMode() {
-
         left = hardwareMap.get(DcMotor.class, "left");
         right = hardwareMap.get(DcMotor.class, "right");
+        right.setDirection(DcMotor.Direction.REVERSE);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -26,17 +31,24 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
         waitForStart();
 
         // run until the end of the match (driver presses STOP)
-        double tgtPower = 0;
-
         while (opModeIsActive()) {
-            tgtPower = -this.gamepad1.left_stick_y;
-            left.setPower(-tgtPower);
-            right.setPower(tgtPower);
-            telemetry.addData("Target Power", tgtPower);
+            yValue = gamepad1.left_stick_y;
+            xValue = gamepad1.left_stick_x;
+
+            leftPower = yValue - xValue;
+            rightPower = yValue + xValue;
+
+            left.setPower(Range.clip(leftPower, -1.0, 1.0));
+            right.setPower(Range.clip(rightPower, -1.0, 1.0));
+
+            telemetry.addData("stick", "  y=" + yValue + "  x=" + xValue);
+            telemetry.addData("power", "  left=" + leftPower + "  right=" + rightPower);
             telemetry.addData("Left Motor Power", left.getPower());
             telemetry.addData("Right Motor Power", right.getPower());
             telemetry.addData("Status", "Running");
             telemetry.update();
+
+            idle();
         }
     }
 }
